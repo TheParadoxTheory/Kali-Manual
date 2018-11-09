ecea62ff17e8fa8772fbe0959b4bf8e1bb3facb5
TheParadoxTheory <44896884+TheParadoxTheory@users.noreply.github.com>
2018-11-09 14:55:15 +0300

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,111 @@
+
+Name = input("Whats Your name? :")
+Age = int(input("Whats your age? :"))
+if Age < 18:
+	print("You are not eligeble for this script")
+	print(".")
+	print(".")
+	print(".")
+	print(".")
+	print(".")
+	print(".")
+	print(".")
+	print(".")
+	print(".")
+	print(".")
+	print(".")
+	print(".")
+	print(".")
+	print(".")
+	print(".")
+	print("Please Leave If the Command Above Was True")
+from math import*
+print("Thank you " + Name + " for using this script")
+print("All rights Reserved ")
+print("█▀▄▀█ █▀▀█ █▀▀▄ █░░█ █▀▀█ █░░")
+print("█░▀░█ █▄▄█ █░░█ █░░█ █▄▄█ █░░")
+print("▀░░░▀ ▀░░▀ ▀░░▀ ░▀▀▀ ▀░░▀ ▀▀▀")
+print("1: Tools")
+print("2: Basic Calc")
+print("3: MITM")
+print("4: Social Engineering")
+print("5: Enable Network Interface")
+print("6: Disable Network Interface")
+print("7: MetaSploit")
+print("8: Scan Networks")
+print("9: View Local Ip")
+print("10: Settings Menu")
+print("11: Local Ips and Gateways")
+Input = int(input("Enter a Number of a specific command: "))
+if Input == 1:
+	print("1: Fluxion")
+	print("2: Wifite")
+	print("3: Morpheus")
+	print("4: Fern")
+	print("5: Reaper")
+	Input_for_1 = float(input("Choose a number: "))
+	if Input_for_1 == 1:
+		print("Paste this into a new terminal\ngit clone https://github.com/wi-fi-analyzer/fluxion.git")
+	if Input_for_1 == 2:
+		print("Paste this into a new terminal\nWifite ")
+	if Input_for_1 == 3:
+		print("Paste this into a new terminal\ngit clone https://github.com/r00t-3xp10it/morpheus.git")
+	if Input_for_1 == 4:
+		print("Paste this into a new terminal\n#cd /pentest/wireless/fern-wifi-cracker python execute.py ")
+	if Input_for_1 == 5:
+		print("Paste this into a new terminal\ngit clone https://github.com/google/REAPER.git")
+if Input == 2:
+	Number2 = float(input("Choose A Number; "))
+	Number3 = float(input("Choose Another Number; "))
+	Operator = input("Choose An Operator; ")
+	if Operator == "+":
+		print(Number2 + Number3)
+	if Operator == "-":
+		print(Number2 - Number3)
+	if Operator == "*":
+		print(Number2 * Number3)
+	if Operator == "/":
+		print(Number2 / Number3)
+if Input == 3:
+	print("Arp Poison")
+	print("Morpheus")
+	print("Ettercap")
+	print("Coming Soon")
+	Input3 = int(input("Enter a number: "))
+	if Input3 == 1:
+		print("https://m.youtube.com/watch?v=RTXAUJ2yqCg")
+	if Input3 == 2:
+		print("Paste this into a new terminal\ngit clone https://github.com/r00t-3xp10it/morpheus.git")
+	if Input3 == 3:
+		print("Applications>Search>Ettercap")
+	if Input3 == 4:
+		print("Sorry, Coming Soon")
+if Input == 4:
+		print("set")
+		final = int(input("Choose"))
+		if final == 1:
+			print("Applications>search>SET")
+if Input == 5:
+	print("Paste this into a new terminal\nifconfig [Network Interface] up")
+if Input == 6:
+	print("Paste this into a new terminal\nifconfig Network Interface Down")
+if Input == 7:
+	print("paste this into a new terminal\nmsfconsole")
+if Input == 8:
+	print("Paste those into a new terminal\nAirmon-ng start wlan0***\nAirodump-ng mon0")
+if Input == 9:
+	print("Paste this into a new terminal\nifconfig")
+if input == 10:
+	print("Network Interface adjustment")
+	print("Exit")
+	Input999 = int(input("Input a number"))
+	if Input999 == 1:
+		Interface = input("Enter an interface")
+	else:
+		print("Press Ctrl + C")
+if Input == 11:
+	print("arp -a for local ips on your network")
+
+
+
+
